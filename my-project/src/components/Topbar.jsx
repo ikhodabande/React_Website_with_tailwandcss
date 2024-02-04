2d2903c7ee26a1791f19bf6a0adfbdfaca8614ea
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-04 17:24:13 +0330

    install tailwand css

---
+++ my-project/src/components/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {AiFillphone, AiOutlineClockCircle} from 'react-icons/ai';
+import {BschatSquareDots} from 'react-icons/bs';
+
+const Topbar = () => {
+  return (
+    <div>
+      <div>
+        <BschatSquareDots/> 
+        
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
