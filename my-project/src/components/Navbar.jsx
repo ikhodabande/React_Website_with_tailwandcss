13a461898e722ef02804e6f9b4cfc1c715691356
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-05 16:18:12 +0330

    adding css styles

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -11,10 +11,16 @@ import {
 
 
 const Navbar = () => {
+  const [nav, setNav]  = useState(false)
+
+  const handleNav = () =>{
+    setNav(!nav)
+  }
+
   return (
-    <div>
-      <ul>
-        <li>
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
+      <ul className='hidden sm:flex px-4'>
+        <li> 
           <a href="#">Home</a>
         </li>
         <li>
@@ -26,12 +32,35 @@ const Navbar = () => {
         <li>
           <a href="#contacts">Contacts</a>
         </li>
-      </ul>
-      <div>
+      </ul> 
+      <div className='flex justify-between'>
         <FaFacebookF className='mx-4'/>
         <FaTwitter className='mx-4'/>
         <FaGooglePlusG className='mx-4'/>
         <FaInstagram className='mx-4'/>
+      </div>
+      {/* Hamberger icons */}
+      <div onClick={handleNav} className='sm:hidden z-10'>
+        <FaBars size={20} className='mr-4 cursor-pointer'/>
+      </div>
+      {/* Mobile menu */}
+      <div 
+      onClick={handleNav}
+      className= { nav ? 'overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 top-0 left-0 w-full h-screen bg-black/90 px-2 py-10 flex flex-col' : 'absolute top-0 h-screen left-[-100%] ease-in duration-500'}>
+      <ul className='w-full h-full text-center pt-12'>
+        <li className='text-2xl py-8'>
+          <a href="#">Home</a>
+        </li>
+        <li className='text-2xl py-8'>
+          <a href="#gallary">Gallary</a>
+        </li>
+        <li className='text-2xl py-8'>
+          <a href="#deals">Deals</a>
+        </li>
+        <li className='text-2xl py-8'>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
       </div>
     </div>
   )
