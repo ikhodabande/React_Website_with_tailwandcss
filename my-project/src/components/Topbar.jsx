e8ee9461535c3b8ed32590d4b5ce96b0a66deb79
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-05 11:15:18 +0330

    addd style for h1

--- my-project/src/components/Topbar.jsx
+++ my-project/src/components/Topbar.jsx
@@ -6,8 +6,8 @@ const Topbar = () => {
   return (
     <div className='flex justify-between items-center px-4 py-2'>
       <div className='flex items-center'>
-        <BsChatSquareDots size={30} className='text-[var(--primary-dark)]'  /> 
-        <h1>WEEKAWAY</h1>
+        <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'  /> 
+        <h1 className='text-xl font-bold text-gray-700'>WEEKAWAY</h1>
       </div>
         <div>
           <AiOutlineClockCircle />
