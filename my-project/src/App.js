b1d5cb0cfc4ddfc3d63ba679972ff7efca58273d
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-05 11:48:43 +0330

    adding navbar to app.js

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Topbar from './components/Topbar';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <Navbar/>
     </div>
   );
 }
