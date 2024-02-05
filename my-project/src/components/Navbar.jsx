fbabade9c9e2ab112619ae338e8dd4e7d1b80b8a
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-05 12:04:00 +0330

    addin navbar icons

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,16 +1,39 @@
 import React, {useState, useEffect} from 'react';
-import {FaBars} from 'react-icons/fa';
 import { BsChatSquareDots } from 'react-icons/bs';
 import {
   FaFacebookF,
   FaTwitter,
   FaGooglePlusG,
   FaInstagram,
+  FaBars
 } from 'react-icons/fa'
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#gallary">Gallary</a>
+        </li>
+        <li>
+          <a href="#deals">Deals</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
+      <div>
+        <FaFacebookF className='mx-4'/>
+        <FaTwitter className='mx-4'/>
+        <FaGooglePlusG className='mx-4'/>
+        <FaInstagram className='mx-4'/>
+      </div>
+    </div>
   )
 }
 
