016cb7a9570bc99371e14fbf73a60bd754a7d486
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 18:39:18 +0330

    adding footer

--- my-project/src/components/Footer.jsx
+++ my-project/src/components/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <div className='max-w-[1140px] w-full py-8 m-auto border-t-4'>
       <div className='flex items-center m-auto justify-center'>
-        <BsChatSquareDots size={30} className='text-[var(--primary-dark)]'/>
-        <h1>WEEKAWAY</h1>
+        <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mx-2'/>
+        <h1 className='font-bold'>WEEKAWAY</h1>
       </div>
     </div>
   )
