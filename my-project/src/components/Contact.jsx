fc8491f2f95639d2c094085db8753a1b2b8f003a
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 18:21:45 +0330

    added style for contact

--- my-project/src/components/Contact.jsx
+++ my-project/src/components/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
       className='w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]'
       src="https://images.unsplash.com/reserve/O7A9fAvYSXC7NTdz8gLQ_IMGP1039.jpg?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
       <form>
-        <div>
+        <div className='grid grid-col-2'>
           <input className='border p-2 m-2' type="text" placeholder='First'/> 
           <input className='border p-2 m-2' type="text" placeholder='Last'/> 
           <input className='border p-2 m-2' type="email" placeholder='Email'/> 
