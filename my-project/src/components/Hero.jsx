0041e74d877a1d393ba9ba25d1d9f18b43b4bd90
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 09:39:16 +0330

    add style for nav

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -4,8 +4,11 @@ import React from 'react';
 function Hero() {
   return (
     <div className='w-full h-[90vh] '>
-      <img src="https://images.unsplash.com/photo-1706650079706-239c03b3cadd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
-    <div>
+      <img src="https://images.unsplash.com/photo-1697462248408-25479e5acfb0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" 
+      className='w-full h-full object-cover'
+      />
+
+    <div className='max-w-[1140px] w-full md:-[50%] max-width-[600px] h-full flex flex-col text-white p-4'>
       <div>
 
         <h1>Take your special offers</h1>
