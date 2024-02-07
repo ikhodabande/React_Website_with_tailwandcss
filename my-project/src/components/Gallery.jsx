cad048242cf4b073a5880696974e5fefdd6060b3
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 15:28:26 +0330

    added imgs

--- my-project/src/components/Gallery.jsx
+++ my-project/src/components/Gallery.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 function Gallery() {
   return (
-    <div>
-      
+    <div id='Gallery'>
+      <h2>Gallery</h2>
+
+      <div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1491555103944-7c647fd857e6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1473172707857-f9e276582ab6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1482356432770-3a99f07aba35?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1527027328672-18f413db15b8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1639323491339-eb7d45b3b022?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+      </div>
+
     </div>
   )
 }
