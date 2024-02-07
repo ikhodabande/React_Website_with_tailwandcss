5d5acd21516cea6efc4f9c31b4225814860a4c28
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 15:16:39 +0330

    we added styles for inputs

--- my-project/src/components/Booking.jsx
+++ my-project/src/components/Booking.jsx
@@ -2,30 +2,30 @@ import React from 'react'
 
 function Booking() {
   return (
-    <div>
-      <form>
-      <div>
+    <div id='deals' className='max-w-[1140px] m-auto w-full p-4'>
+      <form className='lg:flex lg:justify-between w-full items-center '>
+      <div className='flex flex-col my-2 py-2'>
         <label>Destination</label>
-        <select name="" id="">\
+        <select className='lg:w-[300px] md:w-full border rounded-md p-2'>
         <option>Grande Antigua</option>
         <option>Key West</option>
         <option>Maldives</option>
         <option>Cozumel</option>
         </select>
       </div>
-      <div>
-        <div>
-          <label>Check-In</label>
-          <input type="date" />
+      <div className='flex w-full'>
+        <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
+          <label >Check-In</label>
+          <input className='border rounded-md p-2' type="date" />
         </div>
-        <div>
+        <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
           <label>Check-Out</label>
-          <input type="date" />
+          <input className='border rounded-md p-2' type="date" />
         </div>
       </div>
-      <div>
+      <div className='flex flex-col w-full my-2 p-2'>
         <label>Search</label>
-        <button>Rates & Availabilities</button>
+        <button className='w-full'>Rates & Availabilities</button>
       </div>
       </form>
     </div>
