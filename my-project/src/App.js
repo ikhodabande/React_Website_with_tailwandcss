245642830d03ef6ccaaa4817440da24a075f6406
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 11:45:37 +0330

    add activities

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Topbar from './components/Topbar';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Activities from './components/Activities';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Topbar/>
       <Navbar/>
       <Hero/>
+      <Activities/>
     </div>
   );
 }
---
+++ my-project/src/components/Activities.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function activities() {
+  return (
+    <div>
+      <div>
+        <h2>Resorts</h2>
+        <img src="https://images.unsplash.com/photo-1503174768371-25049f8bf3f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
+      </div>
+      <div>
+        <h2>Resorts</h2>
+        <img src="https://images.unsplash.com/photo-1496692952400-80a39ee64b09?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
+      </div>
+      <div>
+        <h2>Resorts</h2>
+        <img src="https://images.unsplash.com/photo-1503023816554-bee7fc3d8442?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
+      </div>
+    </div>
+  )
+}
+
+export default activities
