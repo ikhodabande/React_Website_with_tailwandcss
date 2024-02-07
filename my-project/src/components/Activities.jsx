d686bb6472292032ad823973f2bce35a6e617274
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 12:27:54 +0330

    add styles for activities

--- my-project/src/components/Activities.jsx
+++ my-project/src/components/Activities.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 
 function Activities() {
   return (
-    <div>
-      <div>
-        <h2>Resorts</h2>
-        <img src="https://images.unsplash.com/photo-1503174768371-25049f8bf3f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
+    <div className='max-w-[1140px] w-full m-auto md:flex mt-[-75px]'>
+      <div className='relative p-4'>
+        <h2 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h2>
+        <img
+        className='w-full h-full border-4 object-cover relative border-white shadow-lg '
+        src="https://images.unsplash.com/photo-1503174768371-25049f8bf3f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
       </div>
-      <div>
-        <h2>Resorts</h2>
-        <img src="https://images.unsplash.com/photo-1496692952400-80a39ee64b09?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
+      <div className='relative p-4'>
+        <h2 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h2>
+        <img
+        className='w-full h-full border-4 object-cover relative border-white shadow-lg '
+        src="https://images.unsplash.com/photo-1496692952400-80a39ee64b09?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
       </div>
-      <div>
-        <h2>Resorts</h2>
-        <img src="https://images.unsplash.com/photo-1503023816554-bee7fc3d8442?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
+      <div className='relative p-4'>
+        <h2 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h2>
+        <img 
+        className='w-full h-full border-4 object-cover relative border-white shadow-lg '
+        src="https://images.unsplash.com/photo-1503023816554-bee7fc3d8442?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" />
       </div>
     </div>
   )
 }
 
-export default Activities
+export default Activities;
