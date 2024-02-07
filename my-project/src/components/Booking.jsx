b9a579b6513fc1d2b991876f17d8ed7ce9bf2072
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-07 12:32:41 +0330

    adding options for booking

---
+++ my-project/src/components/Booking.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Booking() {
+  return (
+    <div>
+      <div>
+        <label>Destination</label>
+        <select name="" id="">\
+        <option>Grande Antigua</option>
+        <option>Key West</option>
+        <option>Maldives</option>
+        <option>Cozumel</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default Booking
