c5bf5854124f56eb295012fd4a5feafd46270384
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-22 11:52:36 +0330

    adding images for gallery

--- my-project/src/components/Gallery.jsx
+++ my-project/src/components/Gallery.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ga1 from '../assetes/ga1.avif';
+import ga2 from '../assetes/ga2.avif';
+import ga3 from '../assetes/ga3.avif';
+import ga4 from '../assetes/ga4.avif';
+import ga5 from '../assetes/ga5.avif';
+
 
 function Gallery() {
   return (
@@ -7,19 +13,19 @@ function Gallery() {
 
       <div className='grid sm:grid-cols-5 gap-4' >
         <div className='sm:col-span-3 col-span-2 row-span-2'>
-          <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1491555103944-7c647fd857e6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img className='w-full h-full object-cover' src={ga1} alt="" />
         </div>
         <div>
-          <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1473172707857-f9e276582ab6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img className='w-full h-full object-cover' src={ga2} alt="" />
         </div>
         <div>
-          <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1482356432770-3a99f07aba35?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img className='w-full h-full object-cover' src={ga3} alt="" />
         </div>
         <div>
-          <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1527027328672-18f413db15b8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img className='w-full h-full object-cover' src={ga4} alt="" />
         </div>
         <div>
-          <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1639323491339-eb7d45b3b022?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img className='w-full h-full object-cover' src={ga5} alt="" />
         </div>
       </div>
 
