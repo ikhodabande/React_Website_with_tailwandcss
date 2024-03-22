02b5deee8d88ce3ec4b8556d3eca2c0d39f6bfb6
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-22 11:47:47 +0330

    adding bg and adding assetes

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import backGround from '../assetes/bg.avif';
 
 function Hero() {
   return (
     <div className='w-full h-[90vh] '>
-      <img src="https://images.unsplash.com/photo-1697462248408-25479e5acfb0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="/" 
+      <img src={backGround} alt="/" 
       className='w-full h-full object-cover'
       />
 
@@ -23,4 +23,4 @@ function Hero() {
   )
 }
 
-export default Hero
+export default Hero;
